63e415f8d9918b9731cec299be5fdd5af04b918a
cyan478 <cyan2@stuy.edu>
2017-04-08 17:27:23 -0400

    whut

--- Congressional/SignInViewController.swift
+++ Congressional/SignInViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
-import Firebase
-
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
 
+    @IBOutlet weak var userEmail: UITextField!
+    
+    @IBOutlet weak var userPassword: UITextField!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
+    var isSignIn: Bool = true // Default is on Sign In
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +31,55 @@ class SignInViewController: UIViewController {
     }
     
 
+    @IBAction func signChanged(_ sender: Any) {
+        isSignIn = !isSignIn
+        
+        if isSignIn{
+            signInButton.setTitle("Login", for: .normal)
+        }
+        else{
+            signInButton.setTitle("Register", for: .normal )
+        }
+    
+    }
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if let email = userEmail.text, let pass = userPassword.text {
+            if isSignIn{
+                FIRAuth.auth()?.signIn(withEmail: email, password: pass, completion: { (user, error) in
+                    
+                    // Check that user isn't nil
+                    if let u = user {
+                        // User is found, go to home screen
+                        self.performSegue(withIdentifier: "goToHome", sender: self)
+                    }
+                    else {
+                        // Error: check error and show message
+                        println( "Error while signing in")
+                    }
+                    
+                })
+                
+            }
+            else {
+                // Register the user with Firebase
+                
+                FIRAuth.auth()?.createUser(withEmail: email, password: pass, completion: { (user, error) in
+                    
+                    if let u = user {
+                        //User is found
+                        
+                    }
+                    else {
+                        println("soemthign when wrong with register")
+                    }
+                })
+
+        }
+    
+    
+    }
     
     
     /*
