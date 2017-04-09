687576433db31e9b29840ced1a92ed3e8796aa21
cyan478 <cyan2@stuy.edu>
2017-04-09 03:57:50 -0400

    signup werks

---
+++ Congressional/AccountGate.swift
@@ -0,0 +1,14 @@
+//
+//  AccountGate.swift
+//  Congressional
+//
+//  Created by Celine Yan on 4/9/17.
+//  Copyright © 2017 Ayman Ahmed. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class AccountGate: UIStoryboardSegue {
+    
+}
---
+++ Congressional/FollowViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FollowViewController.swift
+//  Congressional
+//
+//  Created by Celine Yan on 4/9/17.
+//  Copyright © 2017 Ayman Ahmed. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class FollowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        FIRAuth.auth()?.addStateDidChangeListener { auth, user in
+            if user == nil {
+                self.performSegue(withIdentifier: "feedGate", sender: self)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
